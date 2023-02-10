529c66a5d57c7c1b1ee9a912849c81b0289d1bcb
JooHo Kim <artipect123@gmail.com>
2023-02-10 13:34:53 +0900

    최종

--- js/results.js
+++ js/results.js
@@ -1,1 +1,26 @@
-import {results}from './data.js'
+import {results, mbtis} from './data.js'
+// mbti 쿼리스트링 가져오기 ( http://~~~ ?mbti=infp)
+
+const mbti = new URLSearchParams(location.search).get('mbti')
+
+const result = results[mbtis[mbti]]
+
+const titleEl= document.querySelector('.page-title')
+const characterEl= document.querySelector('.character')
+const boxEls= document.querySelectorAll('.box')
+const jobEls= document.querySelectorAll('.job')
+const lectureEl= document.querySelector('.lecture')
+const lectureImgEl= document.querySelector('.lecture img')
+
+titleEl.innerHTML = result.title
+characterEl.src= result.character
+boxEls.forEach(function(boxEl, index){
+  boxEl.innerHTML = result.results[index]
+})
+jobEls.forEach(function(jobEl, index){
+  jobEl.innerHTML = result.jobs[index]
+})
+
+lectureEl.href = result.lectureUrl
+
+lectureImgEl.src= result.lectureImg
